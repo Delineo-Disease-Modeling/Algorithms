2910a3eb029a4a3c4375a54e93ed145f811c755f
Stick <20114698+stickms@users.noreply.github.com>
2025-04-21 10:29:52 -0400

    Added data streaming

--- server/server.py
+++ server/server.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, Response
 from flask_cors import CORS, cross_origin
 from czcode import generate_cz
 from popgen import gen_pop
 from patterns import gen_patterns
 from datetime import datetime
-import json
 import threading
 import requests
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -33,6 +33,39 @@ def gen_and_upload_data(geoids, czone_id):
   else:
     print('error sending data...')
 
+def create_cz(data):
+  yield ''
+  
+  geoids, map = generate_cz(data['cbg'], data['min_pop'])
+
+  cluster = list(geoids.keys())
+  size = sum(list(geoids.values()))
+    
+  resp = requests.post('http://localhost:1890/convenience-zones', json={
+    'name': data['name'],
+    'latitude': map.location[0],
+    'longitude': map.location[1],
+    'cbg_list': cluster,
+    'start_date': data['start_date'],
+    'size': size
+  })
+    
+  if not resp.ok:
+    return '{}'
+  
+  czone_id = resp.json()['data']['id']
+  
+  thread = threading.Thread(target=gen_and_upload_data, args=(geoids, czone_id))
+  thread.daemon = True
+  thread.start()
+  
+  yield json.dumps({
+    'id': czone_id,
+    'cluster': cluster,
+    'size': size,
+    'map': map._repr_html_()
+  })
+
 @app.route('/generate-cz', methods=['POST'])
 @cross_origin()
 def route_generate_cz():
@@ -44,37 +77,7 @@ def route_generate_cz():
   if not request.json:
     return make_response(jsonify({'message': 'Please specify a CBG, location name, start date, and minimum population'}), 400)
   
-  geoids, map = generate_cz(request.json['cbg'], request.json['min_pop'])
-
-  cluster = list(geoids.keys())
-  size = sum(list(geoids.values()))
-    
-  resp = requests.post('http://localhost:1890/convenience-zones', json={
-    'name': request.json['name'],
-    'latitude': map.location[0],
-    'longitude': map.location[1],
-    'cbg_list': cluster,
-    'start_date': request.json['start_date'],
-    'size': size
-  })
-    
-  if not resp.ok:
-    return make_response(jsonify({
-      'message': 'Could not upload cluster to database'  
-    }), 500)
-  
-  czone_id = resp.json()['data']['id']
-  
-  thread = threading.Thread(target=gen_and_upload_data, args=(geoids, czone_id))
-  thread.daemon = True
-  thread.start()
-  
-  return jsonify({
-    'id': czone_id,
-    'cluster': cluster,
-    'size': size,
-    'map': map._repr_html_()
-  })
+  return Response(create_cz(request.json.copy()), mimetype='text/plain')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=1880)
